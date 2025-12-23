76977f39e17890a17574da7f221c26159f87d781
woneung-lee <85263184+woneung-lee@users.noreply.github.com>
2025-12-23 13:44:37 +0900

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -3,12 +3,12 @@
 // https://console.firebase.google.com/
 
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_PROJECT_ID.appspot.com",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyAXpp13ARkgrGMHBvccZI5WMz5S3FlC9gw",
+  authDomain: "lunchbox-d547e.firebaseapp.com",
+  projectId: "lunchbox-d547e",
+  storageBucket: "lunchbox-d547e.firebasestorage.app",
+  messagingSenderId: "726812509012",
+  appId: "1:726812509012:web:ec734f89aa3a9f8c15bec1"
 };
 
 // Firebase 초기화
